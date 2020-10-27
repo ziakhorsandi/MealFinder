72eccf65784906a3ea05f6240ce6426fb67575b3
monix8899 <monire.moayedi@gmail.com>
2020-10-27 20:39:26 +0330

    first page complited

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,26 @@ document.querySelector(".next").addEventListener("click", () => mySiema.next());
 
 const mySiema2 = new Siema({
   selector: ".siema2",
-  perPage: 4,
+  perPage: {
+    100: 3,
+    375: 4,
+    576: 5,
+    768: 6,
+    // 800: 5,
+    1000: 6,
+    1440: 7,
+  },
   // loop: true,
 });
+
+// const mySiema2 = new Siema({
+//   selector: ".siema2",
+//   perPage: {
+//     375: 3,
+//     576: 4,
+//     800: 5,
+//     1000: 6,
+//     1440: 7,
+//   },
+//   // loop: true,
+// });
